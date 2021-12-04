3f3026dc5cdd14754d95b9a6d2a7b7adf9c047c3
Lukas Steinbrecher <lukas.steinbrecher@senacor.com>
2021-12-04 13:12:20 +0100

    Up

--- js/dayX.test.js
+++ js/dayX.test.js
@@ -4,20 +4,20 @@ import { readInput } from "./util";
 const DAY = import.meta.url.replace(/\..+/, "").replace(/^.+\//, "");
 console.log("Running " + DAY);
 
-describe("Solution 1",()=>{
+describe("Solution 1", () => {
     function solution(input) {
         const xs = input.split("\n")
             .map(x => x.trim())
             .map(x => {
                 const r = /(.+?) (.+?) (.+?) (.+?)/.exec(x);
-                if(!r){
+                if (!r) {
                     console.error(`Regex did not match for "${x}"`);
                 }
             })
             .map((x) => {
                 let [matched, x1, x2, x3, x4] = x;
 
-                console.log({x1,x2,x3,x4});
+                console.log({x1, x2, x3, x4});
 
                 return [+x1];
             });
@@ -27,28 +27,28 @@ describe("Solution 1",()=>{
         return xs;
     }
 
-    it("Test", ()=>{
+    it("Test", () => {
         expect(solution(`1 2 3 4
         4 5 6 7`)).toBe(4512)
     })
 
-    it("Run", ()=> {
-        expect(solution(readInput(`inputs/${DAY}.txt`))).toBe(13);
+    it("Run", () => {
+        console.log(`Solution for ${DAY} is: ` + solution(readInput(`inputs/${DAY}.txt`)))
     })
 });
 
 
-describe("Solution 2",()=>{
+describe("Solution 2", () => {
     function solution(input) {
 
     }
 
-    it("Test", ()=>{
+    it("Test", () => {
         expect(solution(`1 2 3 4
         4 5 6 7`)).toBe(4512)
     })
 
-    it("Run", ()=> {
-        expect(solution(readInput(`inputs/${DAY}.txt`))).toBe(13);
+    it("Run", () => {
+        console.log(`Solution for ${DAY} is: ` + solution(readInput(`inputs/${DAY}.txt`)))
     })
 });
